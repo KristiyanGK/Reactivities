6cd3c8ef5cea250fd86f68e91acc4ddd157c149b
Kristiyan Kamburov <kristiangk98@gmail.com>
2019-11-18 22:19:52 +0200

    Added the frontend crud operations. Removed some unnecessary files. Added uuid package for generating guid for id.

---
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Item, Button, Label, Segment } from "semantic-ui-react";
+import { IActivity } from "../../../app/models/activity";
+
+interface IProps {
+  activities: IActivity[];
+  selectActivity: (id: string) => void;
+  deleteActivity: (id: string) => void;
+}
+
+export const ActivityList: React.FC<IProps> = ({
+  activities,
+  selectActivity,
+  deleteActivity
+}) => {
+  return (
+    <Segment clearing>
+      <Item.Group divided>
+        {activities.map(activitiy => (
+          <Item key={activitiy.id}>
+            <Item.Content>
+              <Item.Header as="a">{activitiy.title}</Item.Header>
+              <Item.Meta>{activitiy.date}</Item.Meta>
+              <Item.Description>
+                <div>{activitiy.description}</div>
+                <div>
+                  {activitiy.city}, {activitiy.venue}
+                </div>
+              </Item.Description>
+              <Item.Extra>
+                <Button
+                  onClick={() => selectActivity(activitiy.id)}
+                  floated="right"
+                  content="View"
+                  color="blue"
+                />
+                <Button
+                  onClick={() => deleteActivity(activitiy.id)}
+                  floated="right"
+                  content="Delete"
+                  color="red"
+                />
+                <Label basic content={activitiy.category} />
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+        ))}
+      </Item.Group>
+    </Segment>
+  );
+};
